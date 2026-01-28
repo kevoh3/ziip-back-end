e2c92eab334f7b29fad47588f22d91a11d8c4fc4
kevoh3 <kelvingachiri91@gmail.com>
2026-01-28 09:52:50 +0300

    error handling

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Callbacks\ChoiceBankCallbackController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\User\AuthController;
 use App\Http\Controllers\Api\User\UserController;
