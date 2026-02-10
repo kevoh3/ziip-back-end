01543e314153cfdb6bb80f5504a6b0aaa285793f
kevoh3 <kelvingachiri91@gmail.com>
2026-02-10 13:14:08 +0300

    user type

---
+++ project/database/migrations/2026_02_10_101223_add_user_type_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('user_type')
+                ->default(1)
+                ->after('id')
+                ->comment('1 = Regular User, 2 = Merchant, 3 = Agent');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
